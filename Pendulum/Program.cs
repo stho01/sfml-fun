15e725b20890b137c762e52afbceae696b598621
Sten Marius <sten.marius.holmen@gmail.com>
2020-01-16 21:10:07 +0100

    Pendulum

---
+++ Pendulum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Stho.SFML.Extensions;
+
+namespace Pendulum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = WindowFactory.CreateDefault();
+            var game = new Game(window);
+            game.Initialize();
+            game.Start();
+        }
+    }
+}
