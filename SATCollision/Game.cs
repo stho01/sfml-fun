092d36297ba13e3b9f06ee932fbbd362c59b8256
Sten Marius <sten.marius.holmen@forse.no>
2021-08-01 21:40:10 +0200

    :lipstick:

--- SATCollision/Game.cs
+++ SATCollision/Game.cs
@@ -13,8 +13,6 @@ namespace SATCollision
         private readonly Box _box1 = new(100, 100);
         private readonly Box _box2 = new(50, 50);
         
-        // private readonly CircleShape _triangle = new CircleShape(10, 3);
-        
         public Game(RenderWindow window) : base(window)
         {
             _boxRenderer = new BoxRenderer(window);
@@ -26,12 +24,7 @@ namespace SATCollision
             ShowFps = true;
             _box1.Position = new Vector2f(WindowWidth/2, WindowHeight/2);
             _box2.Position = new Vector2f(100, 100);
-
-            // for (uint i = 0; i < _triangle.GetPointCount(); i++)
-            //     Console.WriteLine(_triangle.GetPoint(i));
-            // _triangle.Position = new Vector2f(100, 100);
-            // for (uint i = 0; i < _triangle.GetPointCount(); i++)
-            //     Console.WriteLine(_triangle.GetPoint(i));
+            
         }
 
         protected override void Update()
@@ -43,7 +36,12 @@ namespace SATCollision
             if (Keyboard.IsKeyPressed(Keyboard.Key.D)) _box2.Rotation -= 360 * Timer.DeltaTimeSeconds;
             
             _boxController.Rotate(_box1, 10 * Timer.DeltaTimeSeconds);
-            _box2.Intersected = _boxController.SatCollision(_box1, _box2);
+            
+            var intersects = _boxController.SatCollision(_box1, _box2);
+            _box1.Intersected = intersects;
+            _box2.Intersected = intersects;
+
+
         }
 
         protected override void Render()
--- SATCollision/SatCollisionResolver.cs
+++ SATCollision/SatCollisionResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using SFML.System;
 using Stho.SFML.Extensions;
 
@@ -8,42 +9,46 @@ namespace SATCollision
     {
         public static bool Intersects(Vector2f[] p, Vector2f[] q)
         {
-            bool PrimaryCollisionCheck(Vector2f[] pPoints, Vector2f[] qPoints)
-            {
-                for (var i = 0; i < pPoints.Length; i++)
-                {
-                    var p1 = pPoints[i];
-                    var p2 = pPoints[(i + 1) % pPoints.Length];
-                    var direction = (p2 - p1);
-                    var normal = new Vector2f(-direction.Y, direction.X);
-
-                    var (pMin, pMax) = GetMinMax(normal, pPoints);
-                    var (qMin, qMax) = GetMinMax(normal, qPoints);
-
-                    if (!(pMax > qMin || qMax < pMin)) 
-                        return false;
-                }
-
-                return true;
-            }
-            
-            (float min, float max) GetMinMax(Vector2f normal, Vector2f[] points)
-            {
-                var max = float.MinValue;
-                var min = float.MaxValue;
-
-                foreach (var point in points)
-                {
-                    max = Math.Max(point.Dot(normal), max);
-                    min = Math.Min(point.Dot(normal), min);
-                }
-
-                return (min, max);
-            }
-
-
             return PrimaryCollisionCheck(p, q) 
                 && PrimaryCollisionCheck(q, p);
+        }
+        
+        /// <summary>Checking collisions for primary</summary>
+        /// <param name="primary"></param>
+        /// <param name="secondary"></param>
+        /// <returns>True if primary points check evaluates true</returns>
+        private static bool PrimaryCollisionCheck(Vector2f[] primary, Vector2f[] secondary)
+        {
+            for (var i = 0; i < primary.Length; i++)
+            {
+                var p1 = primary[i];
+                var p2 = primary[(i + 1) % primary.Length];
+                var edge = (p2 - p1);
+                var normal = new Vector2f(-edge.Y, edge.X);
+
+                var (pMin, pMax) = GetMinMax(normal, primary);
+                var (qMin, qMax) = GetMinMax(normal, secondary);
+
+                if (!(pMax < qMin || qMax > pMin)) 
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static (float min, float max) GetMinMax(Vector2f normal, IEnumerable<Vector2f> points)
+        {
+            var max = float.MinValue;
+            var min = float.MaxValue;
+
+            foreach (var point in points)
+            {
+                var dot = point.Dot(normal);
+                max = Math.Max(dot, max);
+                min = Math.Min(dot, min);
+            }
+
+            return (min, max);
         }
     }
 }
