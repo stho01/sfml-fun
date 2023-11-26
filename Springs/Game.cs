716ba916af3d0bd139eba1b3a6c4901f8e73eabd
stho <sten.marius.holmen@gmail.com>
2023-11-26 20:55:59 +0100

    Springs

---
+++ Springs/Game.cs
@@ -0,0 +1,48 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using Stho.SFML.Extensions;
+
+namespace Springs;
+
+public class Game(RenderWindow window) 
+    : GameBase(window)
+{
+    private readonly Spring _spring = new();
+    private readonly Bob _bob = new();
+    
+    public override void Initialize()
+    {
+        _spring.Anchor = new Vector2f(WindowWidth/ 2f, 100f);
+        _spring.Length = 190f;
+        _spring.K = 60f;
+        _bob.Position = new Vector2f(WindowWidth / 2f, 200f);
+        _bob.Shape.Radius = 30f;
+    }
+
+    protected override void Update()
+    {
+        if (Keyboard.IsKeyPressed(Keyboard.Key.Space))
+            _bob.ApplyForce(5000f, 0f);
+        
+        _bob.ApplyForce(0, 5000f);
+        
+        _spring.Connect(_bob);
+        _bob.Update();
+
+        if (Mouse.IsButtonPressed(Mouse.Button.Left))
+        {
+            var pos = Mouse.GetPosition(Window);
+            if (_bob.Shape.GetGlobalBounds().Contains(pos.X, pos.Y))
+            {
+                _bob.Position = (Vector2f)pos;
+            }
+        }
+    }
+
+    protected override void Render()
+    {
+        _spring.Render(window);
+        _bob.Render(Window);
+    }
+}
---
+++ Springs/Program.cs
@@ -0,0 +1,7 @@
+﻿using Springs;
+using Stho.SFML.Extensions;
+
+var window = WindowFactory.CreateDefault();
+var game = new Game(window);
+game.Initialize();
+game.Start();
